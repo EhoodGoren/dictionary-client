08feb0ff823b68714a4a7919d65f9a33a22c4090
EhoodGoren <ehoodgoren@gmail.com>
2022-01-21 14:13:10 +0200

    Added route for searching a word by the word and a part of speech

--- backend/routers/wordRouter.js
+++ backend/routers/wordRouter.js
@@ -1,10 +1,29 @@
-const { response } = require('express');
 const express = require('express');
 const docClient = require('../awsConnection');
 
 const router = express.Router();
 
 const table = 'dictionary';
+
+router.get('/:word/:partOfSpeech', async (req, res) => {
+    const { word, partOfSpeech } = req.params;
+    const params = {
+        TableName: table,
+        Key: {
+            word,
+            "part_of_speech": partOfSpeech
+        }
+    };
+    try {
+        const wordMatch = await docClient.get(params).promise();
+        wordMatch.Item ? 
+            res.send(wordMatch.Item) :
+            res.status(404).send("Word doesn't exist");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Try again');
+    }
+})
 
 router.get('/:word', async (req, res) => {
     const { word } = req.params;
