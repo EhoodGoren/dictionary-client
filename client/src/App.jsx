de38bb5915c354d668d317679546498c84f33e78
EhoodGoren <ehoodgoren@gmail.com>
2022-01-22 13:24:55 +0200

    Added route and component for handling search of a word + part of speech

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import WordSearch from './WordSearch';
 import WordDefinition from './WordDefinition';
+import SpecificWord from './SpecificWord';
 
 const App = () => {
     return(
@@ -10,6 +11,10 @@ const App = () => {
                 <Route
                     path='/'
                     element={<WordSearch /> }
+                />
+                <Route
+                    path={'/:word/:partOfSpeech'}
+                    element={<SpecificWord />}
                 />
                 <Route
                     path={'/:word'}
--- client/src/SpecificWord.jsx
+++ client/src/SpecificWord.jsx
@@ -2,38 +2,39 @@ import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
 
-const WordDefinition = () => {
-    const { word } = useParams();
-    const [searchData, setSearchData] = useState([]);
+const SpecificWord = () => {
+    const { word, partOfSpeech } = useParams();
+    const [searchData, setSearchData] = useState({});
 
     useEffect(() => {
         const searchWord = async () => {
             try {
-                const response = await axios.get(`http://localhost:8080/${word}`);
+                const response = await axios.get(`http://localhost:8080/${word}/${partOfSpeech}`);
                 setSearchData(response.data);
             } catch (error) {
                 setSearchData('');
             }
         }
         searchWord();
-    }, [word]);
+    }, [word, partOfSpeech]);
 
-    const generateDefinitions = () => {
+    const generateDefinition = () => {
         if(searchData === '') return <div>No matches</div>
-        return searchData.map(({ word, part_of_speech, definition }, index ) => (
-            <div key={`definition${index+1}`}>
+        const { word, part_of_speech, definition } = searchData;
+        return (
+            <div>
                 <h1>{word}</h1>
                 <h2>{part_of_speech}</h2>
                 <div>{definition}</div>
             </div>
-        ));
+        )
     }
-
+    
     return(
         <div>
-            {generateDefinitions()}
+            {generateDefinition()}
         </div>
     )
 }
 
-export default WordDefinition;
+export default SpecificWord;
