9f7a8af1bcea4e771d0d3977c97e096c20231497
EhoodGoren <ehoodgoren@gmail.com>
2022-01-22 16:55:48 +0200

    Selecting a part of speech with a word to search now focuses the search to a single matching result

--- client/src/WordSearch.jsx
+++ client/src/WordSearch.jsx
@@ -4,21 +4,23 @@ import './WordSearch.css';
 
 const WordSearch = () => {
     const wordInput = useRef();
+    const partInput = useRef();
     const navigate = useNavigate();
 
     const searchWord = async (e) => {
         e.preventDefault();
         const currentInput = wordInput.current.value;
+        if(partInput.current.value) return navigate(`/${currentInput}/${partInput.current.value}`);
         navigate(`/${currentInput}`);
     }
     return (
         <form onSubmit={searchWord}>
             <div className="search-bar">
-                <select className='part-select'>
+                <select className='part-select' ref={partInput}>
                     <option value=''></option>
-                    <option value='Noun'>Noun</option>
-                    <option value='Verb'>Verb</option>
-                    <option value='Adjective'>Adjective</option>
+                    <option value='noun'>noun</option>
+                    <option value='verb'>verb</option>
+                    <option value='adjective'>adjective</option>
                 </select>
                 <input className='search-inputs' ref={wordInput} placeholder='Search a word' required />
                 <button type='submit' className='search-button'>Search</button>
