f0248d53e253af91ba00a89ac0be68b7e101a5f9
EhoodGoren <ehoodgoren@gmail.com>
2022-01-22 14:36:54 +0200

    Styled search bar

--- client/src/WordSearch.jsx
+++ client/src/WordSearch.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { useNavigate } from 'react-router-dom';
+import './WordSearch.css';
 
 const WordSearch = () => {
     const wordInput = useRef();
@@ -12,8 +13,16 @@ const WordSearch = () => {
     }
     return (
         <form onSubmit={searchWord}>
-            <input ref={wordInput} placeholder='Search a word' required />
-            <button type='submit'>Search</button>
+            <div className="search-bar">
+                <select className='part-select'>
+                    <option value=''></option>
+                    <option value='Noun'>Noun</option>
+                    <option value='Verb'>Verb</option>
+                    <option value='Adjective'>Adjective</option>
+                </select>
+                <input className='search-inputs' ref={wordInput} placeholder='Search a word' required />
+                <button type='submit' className='search-button'>Search</button>
+            </div>
         </form>
     )
 }
