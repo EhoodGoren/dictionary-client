a550195cf7695b94375079a1276593d6be841732
EhoodGoren <ehoodgoren@gmail.com>
2022-01-23 00:29:17 +0200

    Added script for uploading all data in batches.
    Also added script for sorting all data before filtering doubles, since it wasn't sorted.

--- backend/dataUpload.js
+++ backend/dataUpload.js
@@ -7,36 +7,45 @@ AWS.config.update({
 
 const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-const params = {
-    RequestItems: {
-        'dictionary': [
-            {
-                PutRequest: {
-                    Item: {
-                        word: { 'S': data[0].word },
-                        part_of_speech: { 'S': data[0].part_of_speech || 'none' },
-                        definition: { 'S': data[0].definition }
-                    }
-                }
-            },
-            {
-                PutRequest: {
-                    Item: {
-                        word: { 'S': data[1].word },
-                        part_of_speech: { 'S': data[1].part_of_speech || 'none' },
-                        definition: { 'S': data[1].definition }
-                    }
-                }
-            }
-        ]
+const batchUploadData = async (uploadData) => {
+    const params = {
+        RequestItems: {
+            'dictionary': uploadData
+        }
     }
-}
-
-const batchUploadData = async () => {
     try {
         await ddb.batchWriteItem(params).promise();
     } catch (error) {
         console.log(error);
     }
 }
-batchUploadData();
+
+const formatData = data.map(({ word, part_of_speech, definition }) => {
+    return {
+        PutRequest: {
+            Item: {
+                word: { 'S': word },
+                part_of_speech: { 'S': part_of_speech || 'none' },
+                definition: { 'S': definition }
+            }
+        }
+    }
+});
+
+const splitData = [];
+for (let i = 0; i < formatData.length; i += 25) {
+    const dataSlice = formatData.slice(i, i + 25);
+    splitData.push(dataSlice);
+}
+
+const uploadAllData = async () => {
+    try {
+        const uploadResponse = await Promise.all(splitData.map(async (dataSlice) => {
+            await batchUploadData(dataSlice);
+        }));
+        console.log(uploadResponse);
+    } catch (error) {
+        console.log(error);   
+    }
+}
+uploadAllData();
