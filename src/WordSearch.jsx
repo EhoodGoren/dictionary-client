87c9c480a1b314a1c013fb52e7dd769f2b252b10
EhoodGoren <ehoodgoren@gmail.com>
2022-02-05 19:25:11 +0200

    Added search mode, now including also a search by part of speech

--- src/WordSearch.jsx
+++ src/WordSearch.jsx
@@ -1,14 +1,16 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import './WordSearch.css';
 
 const WordSearch = () => {
+    const [searchMode, setSearchMode] = useState('word')
     const wordInput = useRef();
     const partInput = useRef();
     const navigate = useNavigate();
 
     const searchWord = async (e) => {
         e.preventDefault();
+        if(searchMode === 'part-of-speech') return navigate(`/part-of-speech/${partInput.current.value}`)
         const currentInput = wordInput.current.value;
         if(partInput.current.value) return navigate(`/${currentInput}/${partInput.current.value}`);
         navigate(`/${currentInput}`);
@@ -16,14 +18,27 @@ const WordSearch = () => {
     return (
         <form onSubmit={searchWord}>
             <div className="search-bar">
-                <select className='part-select' ref={partInput}>
+                <select className='part-select' ref={partInput} required={searchMode === 'part-of-speech' ? true : false}>
                     <option value=''></option>
-                    <option value='noun'>noun</option>
-                    <option value='verb'>verb</option>
-                    <option value='adjective'>adjective</option>
+                    <option value='n.'>noun</option>
+                    <option value='v.'>verb</option>
+                    <option value='a.'>adjective</option>
+                    <option value='adv.'>adverb</option>
+                    <option value='prep.'>preposition</option>
                 </select>
-                <input className='search-inputs' ref={wordInput} placeholder='Search a word' required />
+                <input className='search-inputs' ref={wordInput} placeholder='Search a word' required disabled={searchMode === 'word' ? false : true}/>
                 <button type='submit' className='search-button'>Search</button>
+            </div>
+            <div className="search-mode">
+                Search by:
+                <label>
+                    <input type='radio' name='search-mode' value='word' onChange={() => setSearchMode('word')}/>
+                    Word
+                </label>
+                <label>
+                    <input type='radio' name='search-mode' value='part-of-speech' onChange={() => setSearchMode('part-of-speech')}/>
+                    Part of speech
+                </label>
             </div>
         </form>
     )
