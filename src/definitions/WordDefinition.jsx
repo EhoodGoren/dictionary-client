a268fcc14ac71959863217dc73655394c9f7890a
EhoodGoren <ehoodgoren@gmail.com>
2022-02-05 20:16:19 +0200

    Each word in definition is now clickable. Clicking it will redirect to search it

--- src/definitions/WordDefinition.jsx
+++ src/definitions/WordDefinition.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import './WordDefinition.css';
 
 const WordDefinition = ({ result: { word, part_of_speech, definition } }) => {
+    const navigate = useNavigate();
+
+    const generateDefinition = (text) => {
+        if (!text) return
+        const splitText = text.split(' ');
+        return splitText.map((textPart, index) => (
+            <span key={`definition-word-${index+1}`}>
+                <span
+                    className="definition-word"
+                    onClick={() => navigateToWord(textPart)}
+                >{textPart}</span>
+                {' '}
+            </span>
+        ));
+    }
+    const navigateToWord = (searchWord) => {
+        const plainWord = searchWord.replace(/[^\w-]/g, '')
+        navigate(`/${plainWord}`)
+    }
     return (
         <div className='results'>
             <h1 className='searched-word'>{word}</h1>
             <h3 className='word-parts'><i>{part_of_speech}</i></h3>
-            <div className='definitions'>{definition}</div>
+            <div className='definitions'>{generateDefinition(definition)}</div>
         </div>
     )
 }
